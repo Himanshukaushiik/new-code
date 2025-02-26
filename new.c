bae20dcaea5effc41eb0da044b21d860afd52ccb
himanshu <himanshukaushiik@gmail.com>
2025-02-26 13:34:05 +0530

    new code file

--- new.c
+++ new.c
@@ -1,1 +1,44 @@
 #include<stdio.h>
+
+void bubblesort(int arr[], int );
+
+
+
+void bubblesort(int arr[] , int n ){
+
+    for (int i = 0; i < n-1; i++)
+    {
+           for (int j = 0; j < n-i-1; j++)
+            {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1] = temp; 
+                }
+            }
+            
+    }
+    
+}
+
+
+int main(){
+
+    int arr[]={55,56,54,23,12,18,99,75,32,53};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    bubblesort(arr , n);
+
+    printf("\nTHE SORTED ARRAY IS = \n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    
+return 0;
+
+}
+
+
+
